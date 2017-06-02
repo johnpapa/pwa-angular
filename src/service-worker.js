526ea426a9056fcbaea041245c9b52c42d036d32
John Papa <john@Johns-MBP.lan>
2017-06-01 22:45:06 -0400

    logging sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 // https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
 
-var CACHE_NAME = 'starwars-api-site-cache-v1';
+var CACHE = 'starwars-api-site-cache-v1';
 var urlsToCache = [
   '/',
   '/index.html',
@@ -12,61 +12,60 @@ var urlsToCache = [
   '/vendor.bundle.js',
   '/main.bundle.js'
 ];
-// var i = 0;
-// function foo() {
-//   console.log('new code detected', i++)
-// }
-// foo();
+
 self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  // Open a cache and use addAll() with an array of assets to add all
+  // of them to the cache. Ask the service worker to keep installing
+  // until the returning promise resolves.
+  event.waitUntil(caches.open(CACHE).then(function (cache) {
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  }));
 });
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        swLog('found in cache', response);
+        return response;
+      }
+      swLog('not found in cache, will fetch', event.request);
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
+      // IMPORTANT: Clone the request. A request is a stream and
+      // can only be consumed once. Since we are consuming this
+      // once by cache and once by the browser for fetch, we need
+      // to clone the response.
+      var fetchRequest = event.request.clone();
 
-        return fetch(fetchRequest).then(
-          function (response) {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                cache.put(event.request, responseToCache);
-              });
-
+      return fetch(fetchRequest).then(
+        function (response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            swLog('fetch was invalid', response);
             return response;
           }
-        ).catch(function (error) {
-          console.error('fetch failed', error);
-        });
-      })
+          swLog('fetch was valid', response);
+
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone();
+
+          caches.open(CACHE)
+            .then(function (cache) {
+              swLog('putting in cache', { request: event.request, response: responseToCache })
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        }
+      ).catch(function (error) {
+        console.error('fetch failed', error);
+      });
+    })
   );
 });
 
