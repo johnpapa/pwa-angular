968d670fe28650ecbc1846af28f8ed2db835cdcc
John Papa <john@Johns-MBP.lan>
2017-06-03 09:05:09 -0400

    more logging.
    offline.html

--- src/sw.js
+++ src/sw.js
@@ -6,17 +6,24 @@
 
 var CACHE = 'starwars-api-site-cache-v1';
 var URLS_TO_CACHE = [
-  './',
-  './0.chunk.js',
-  './1.chunk.js',
-  './styles.bundle.js',
-  './inline.bundle.js',
-  './polyfills.bundle.js',
-  './vendor.bundle.js',
-  './main.bundle.js'
+  '/',
+  '/offline.html',
+  '/0.chunk.js',
+  '/1.chunk.js',
+  '/styles.bundle.js',
+  '/inline.bundle.js',
+  '/polyfills.bundle.js',
+  '/vendor.bundle.js',
+  '/main.bundle.js'
 ];
 
 self.addEventListener('install', function (event) {
+  // We can kick the old version ou,
+  // but then we're controlling a page loaded
+  // with an older version of a service worker.
+  // But instead, use Update on Reload for development
+  // self.skipWaiting();
+
   // Ask the service worker to keep installing
   // until the returning promise resolves.
   event.waitUntil(preCache());
@@ -51,9 +58,24 @@ self.addEventListener('fetch', function (event) {
 function fromCache(request) {
   swLog('searching the cache for ' + request.url);
   return caches.open(CACHE).then(function (cache) {
+    console.log('trying to match', request);
     return cache.match(request).then(function (matching) {
-      return matching; // || Promise.reject('no-match');
-    });
+      // The match() method of the Cache interface returns a Promise
+      // that resolves to the Response associated with the
+      // first matching request in the Cache object.
+      // If no match is found, the Promise resolves to undefined.
+      if (matching) {
+        swLog('found a match', matching);
+        return matching; // || Promise.reject('no-match');
+      } else {
+        swLog('match not found. request mode ===  ' + request.mode);
+        if (request.mode === 'navigate') {
+          swLog('return offline html');
+          return caches.match('/offline.html');
+        }
+        swLog('no match found and not navigating, so we return undefined');
+      }
+    })
   });
 }
 
@@ -80,19 +102,18 @@ self.addEventListener('activate', function (event) {
   // The following code would do this by looping through all of the
   // caches in the service worker and deleting any caches that
   // aren't defined in the cache whitelist.
-  var cacheWhitelist = ['starwars-api-site-cache-v1', 'some-other-posts-cache-v1'];
+  var expectedCaches = [CACHE, 'some-other-cache-v1'];
 
   // we use waitUntil() to prevent the worker
   // to be killed until the cache is updated.
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+      return Promise.all(cacheNames.map(function (cacheName) {
+        if (!expectedCaches.includes(cacheName)) {
+          swLog('deleting cache: ' + cacheName);
+          return caches.delete(cacheName);
+        }
+      }));
     })
   );
 });
