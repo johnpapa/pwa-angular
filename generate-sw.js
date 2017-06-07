77fe49dc1514b0223e057ea8286a4aff5f6852a5
John Papa <john@Johns-MBP.lan>
2017-06-07 12:48:59 -0400

    generating sw

--- generate-sw.js
+++ generate-sw.js
@@ -3,7 +3,6 @@ const SRC_DIR = 'src';
 const BUILD_DIR = 'dist';
 
 const input = {
-  swSrc: `${SRC_DIR}/sw.js`,
   swDest: `${BUILD_DIR}/sw.js`,
   globDirectory: BUILD_DIR,
   globPatterns: [
@@ -15,9 +14,25 @@ const input = {
     'index.js',
     'sw.js'
   ],
+  navigateFallback: '/index.html',
+  clientsClaim: true,
+  runtimeCaching: [],
+  handleFetch: true,
   maximumFileSizeToCacheInBytes: 4000000
 };
 
-workboxBuild.injectManifest(input).then(() => {
+workboxSW.router.registerRoute(
+  /\/api\/(.*)/,
+  // workboxSW.strategies.networkFirst({ networkTimeoutSeconds: 1 })
+  workboxSW.strategies.staleWhileRevalidate({ cacheName: 'hero-api' })
+);
+
+const networkFirstStrategy = workboxSW.strategies.networkFirst();
+workboxSW.router.registerRoute('/home/', networkFirstStrategy);
+workboxSW.router.registerRoute('/heroes/', networkFirstStrategy);
+workboxSW.router.registerRoute('/villains/', networkFirstStrategy);
+
+
+workboxBuild.generateSW(input).then(() => {
   console.log('The production service worker has been injected with a precache list.');
 });
